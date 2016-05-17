ee2a92cb1d387f4b671860aae63ae1a45c59a611
TarasSobchyshak <sobchyshak@gmail.com>
2016-05-17 12:12:29 +0300

    Companies relations Web API created.

---
+++ CompaniesRelations/CompaniesRelations.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CompaniesRelations.API.WebApiApplication" Language="C#" %>
---
+++ CompaniesRelations/CompaniesRelations.API/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CompaniesRelations.API.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AnnualEarnings { get; set; }
+
+
+        /*
+            int CalcEarnings(){
+                int res = AnnualEarnings;
+                foreach(var x in SubsidiaryCompanies)
+                {
+                    res += CalcEarnings(x);
+                }
+                return res;
+            }
+
+            */
+        public List<Company> SubsidiaryCompanies { get; set; }
+    }
+}
